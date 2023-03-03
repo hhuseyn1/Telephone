09ce15e0bf0be370511416f5bb76ff9376012888
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-04 00:55:36 +0400

    Make a Design

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -4,8 +4,24 @@ namespace Source;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void UpBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void AddBtn_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
