1ffd535a90fd174b4f0499d47487f62a4f433c05
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-04 02:19:14 +0400

    Add functions for move buttons

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -1,27 +1,83 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Source;
 
 public partial class MainWindow : Window
 {
-
+    public List<string> ListWords { get; set; }
+    public ObservableCollection<string> CloseWords { get; set; }
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void UpBtn_Click(object sender, RoutedEventArgs e)
-    {
-
+        DataContext = this;
+        CloseWords = new();
+        ListWords = new()
+        {
+            "Step IT Academy",
+            "Computer Academy",
+            ".Net",
+            "C# the best",
+            "My name is Huseyn"
+        };
+        Textline.Text = "Step";
+        CheckWord();
     }
 
     private void CBtn_Click(object sender, RoutedEventArgs e)
     {
-
+        Textline.Text = string.Empty;
     }
 
     private void AddBtn_Click(object sender, RoutedEventArgs e)
     {
+        if (!ListWords.Contains(Textline.Text))
+            ListWords.Add(Textline.Text);
+        else
+            MessageBox.Show($"{Textline.Text} is also exist in your List","Information",MessageBoxButton.OK,MessageBoxImage.Information);
+    }
 
+    private void CheckWord()
+    {
+        Task.Run(() =>
+        {
+            Dispatcher.Invoke(() =>
+            {
+                CloseWords.Clear();
+                if (string.IsNullOrEmpty(Textline.Text) && string.IsNullOrWhiteSpace(Textline.Text)) return;
+                foreach (var item in ListWords)
+                {
+                    if (item.ToLower().StartsWith(Textline.Text.ToLower()) && Textline.Text != item)
+                        CloseWords.Add(item);
+                }
+
+            });
+        });
+    }
+
+    private void UDRLBtn_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn)
+        {
+            if (btn.Name == "UpBtn" && ScreenLbox.SelectedIndex > 0)
+                ScreenLbox.SelectedIndex--;
+
+            else if (btn.Name == "DownBtn" && ScreenLbox.SelectedIndex < ScreenLbox.Items.Count-1)
+                ScreenLbox.SelectedIndex++;
+
+            else if(btn.Name == "RightBtn")
+                if (CloseWords.Count > 0)
+                    Textline.Text = ScreenLbox.SelectedItem.ToString();
+
+            CheckWord();
+        }
+    }
+
+    private void Textline_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        CheckWord();
     }
 }
